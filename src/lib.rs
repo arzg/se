b23c9727169537b5de9c38bf5b576acc389b100d
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-13 20:05:24 +1000

    Stop printing user input

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,10 @@ pub fn process_keypress(key_event: KeyEvent) -> ControlFlow {
     let KeyEvent { code, modifiers } = key_event;
 
     if code == KeyCode::Char('q') && modifiers == KeyModifiers::CONTROL {
-        return ControlFlow::Break;
+        ControlFlow::Break
+    } else {
+        ControlFlow::Continue
     }
-
-    if let KeyCode::Char(c) = code {
-        let mut buf = [0; 4];
-        c.encode_utf8(&mut buf);
-
-        println!("{:?} ('{}')\r", buf, c);
-    }
-
-    ControlFlow::Continue
 }
 
 pub enum ControlFlow {
