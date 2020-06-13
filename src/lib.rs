336f16c8c4571bebd859d600d6d8e6eccaa6356a
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-13 20:13:53 +1000

    Clear the screen

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 #![warn(rust_2018_idioms)]
 
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use crossterm::{execute, terminal};
+use std::io::{self, Write};
+
+pub fn refresh_screen(stdout: &mut io::Stdout) -> anyhow::Result<()> {
+    execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
+
+    Ok(())
+}
 
 pub fn process_keypress(key_event: KeyEvent) -> ControlFlow {
     let KeyEvent { code, modifiers } = key_event;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use crossterm::{
-    event::{self, Event},
-    terminal,
-};
+use crossterm::event::{self, Event};
+use crossterm::terminal;
+use std::io;
 
 fn main() -> anyhow::Result<()> {
     terminal::enable_raw_mode()?;
 
+    let mut stdout = io::stdout();
+
+    se::refresh_screen(&mut stdout)?;
+
     loop {
         if let Event::Key(key_event) = event::read()? {
+            se::refresh_screen(&mut stdout)?;
+
             if let se::ControlFlow::Break = se::process_keypress(key_event) {
                 break;
             }
