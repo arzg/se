2e950e043164d02630bf5262712acdb736eb148e
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-13 20:34:33 +1000

    Draw tildes on left of screen

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
 #![warn(rust_2018_idioms)]
 
-use crossterm::{cursor, event, execute, terminal};
+use crossterm::{cursor, event, queue, terminal};
 use std::io::{self, Write};
 
 pub fn refresh_screen(stdout: &mut io::Stdout) -> anyhow::Result<()> {
-    execute!(
+    queue!(
         stdout,
         terminal::Clear(terminal::ClearType::All),
         cursor::MoveTo(0, 0)
     )?;
+
+    draw_rows(stdout)?;
+
+    queue!(stdout, cursor::MoveTo(0, 0))?;
+
+    stdout.flush()?;
+
+    Ok(())
+}
+
+fn draw_rows(stdout: &mut io::Stdout) -> anyhow::Result<()> {
+    // We don’t know how high the terminal is at the moment, so we just default to 24 characters.
+    for _ in 0..24 {
+        writeln!(stdout, "~\r")?;
+    }
 
     Ok(())
 }
