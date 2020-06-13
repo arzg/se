d5df9326c7afba0c8100c63961cec96cb3d3cfe7
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-13 20:22:47 +1000

    Clear the screen if an error occurs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 use crossterm::event::{self, Event};
-use crossterm::terminal;
-use std::io;
+use crossterm::{cursor, execute, terminal};
+use std::io::{self, Write};
 
 fn main() -> anyhow::Result<()> {
     terminal::enable_raw_mode()?;
 
     let mut stdout = io::stdout();
 
-    se::refresh_screen(&mut stdout)?;
+    if let Err(e) = run(&mut stdout) {
+        execute!(
+            stdout,
+            terminal::Clear(terminal::ClearType::All),
+            cursor::MoveTo(0, 0)
+        )?;
+
+        eprintln!("{:?}", e);
+    }
+
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+}
+
+fn run(stdout: &mut io::Stdout) -> anyhow::Result<()> {
+    se::refresh_screen(stdout)?;
 
     loop {
         if let Event::Key(key_event) = event::read()? {
-            se::refresh_screen(&mut stdout)?;
+            se::refresh_screen(stdout)?;
 
             if let se::ControlFlow::Break = se::process_keypress(key_event) {
                 break;
             }
         }
     }
-
-    terminal::disable_raw_mode()?;
 
     Ok(())
 }
